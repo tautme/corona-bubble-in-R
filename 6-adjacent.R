02ac7a7f5b2b2bbcfe77d3ed3a9a7cf42bd1f34f
AdamUArk <jahughes@uark.edu>
2020-04-02 10:29:57 -0500

    got answer from linog on stackoverflow

--- 6-adjacent.R
+++ 6-adjacent.R
@@ -19,67 +19,91 @@ adj_co <- read_tsv("raw_data/county_adjacency.txt", col_names = FALSE,
 
 adj_co <- adj_co %>% mutate(UID = row_number(), adj.logic = is.na(X1))
 adj_co <- adj_co  %>% select(-X1, -X3)
-head(adj_co, 22) %>% as.data.frame()
+# head(adj_co, 22) %>% as.data.frame()
 names(adj_co) <- c("fips", "fips.adjacent", "UID", "adj.logic")
 adj_co <- fill(adj_co, fips, .direction = "down")
+adj_co <- as.data.frame(adj_co)
 # write_csv(adj_co, "data/adjacent_fips_county_usa.csv")
 
 ## rearrange #####
 # adj_co %>% spread(fips, fips.adjacent)
 adj_co
-adj_co %>% group_by(fips)
+# adj_co %>% group_by(fips)
 
 
-# ## stackoverflow question #######
-# dat <- read.table(header=TRUE, stringsAsFactors=TRUE, text="
-#                         head          child UID     logic
-#                     1  01001         01001   1     FALSE
-#                     2  01001         01021   2      TRUE
-#                     3  01001         01047   3      TRUE
-#                     4  01001         01051   4      TRUE
-#                     5  01001         01085   5      TRUE
-#                     6  01001         01101   6      TRUE
-#                     7  01003         01003   7     FALSE
-#                     8  01003         01025   8      TRUE
-#                     9  01003         01053   9      TRUE
-#                     10 01003         01097  10      TRUE
-#                     11 01003         01099  11      TRUE
-#                     12 01003         01129  12      TRUE
-#                     13 01003         12033  13      TRUE
-#                     14 01005         01005  14     FALSE
-#                     15 01005         01011  15      TRUE
-#                     16 01005         01045  16      TRUE
-#                     17 01005         01067  17      TRUE
-#                     18 01005         01109  18      TRUE
-#                     19 01005         01113  19      TRUE
-#                     20 01005         13061  20      TRUE
-#                     21 01005         13239  21      TRUE
-#                     22 01005         13259  22      TRUE")
-# 
-# dat
-# dat %>% class()
-# 
-# # make graph ##########
-# library(tidyverse)
-# library(igraph)
-# library(jsonlite)
-# gdat <- select(dat, head, child)
-# mdat <- as.matrix(gdat)
-# edge_dat <- graph_from_edgelist(mdat)
+## stackoverflow question #######
+dat <- read.table(header=TRUE, stringsAsFactors=TRUE, text="
+                        head          child UID     logic
+                    1  01001         01001   1     FALSE
+                    2  01001         01021   2      TRUE
+                    3  01001         01047   3      TRUE
+                    4  01001         01051   4      TRUE
+                    5  01001         01085   5      TRUE
+                    6  01001         01101   6      TRUE
+                    7  01003         01003   7     FALSE
+                    8  01003         01025   8      TRUE
+                    9  01003         01053   9      TRUE
+                    10 01003         01097  10      TRUE
+                    11 01003         01099  11      TRUE
+                    12 01003         01129  12      TRUE
+                    13 01003         12033  13      TRUE
+                    14 01005         01005  14     FALSE
+                    15 01005         01011  15      TRUE
+                    16 01005         01045  16      TRUE
+                    17 01005         01067  17      TRUE
+                    18 01005         01109  18      TRUE
+                    19 01005         01113  19      TRUE
+                    20 01005         13061  20      TRUE
+                    21 01005         13239  21      TRUE
+                    22 01005         13259  22      TRUE")
+
+dat
+dat %>% class()
+
+# make graph ##########
+library(tidyverse)
+library(igraph)
+library(jsonlite)
+gdat <- select(dat, head, child)
+mdat <- as.matrix(gdat)
+edge_dat <- graph_from_edgelist(mdat)
 # plot.igraph(edge_dat)
-# jdat <- toJSON(mdat, matrix = "rowmajor")
-# ##
-# ##
-# ##
+jdat <- toJSON(mdat, matrix = "rowmajor")
 
+library(data.table)
+setDT(dat)
+dat %>% class()
+# dat_child <- dat[(logic)]
+dat_child <- dat
+dat_child %>% class()
+dat_child <- dat_child[,.(list(unique(child))), by = "head"]
+dat_child[1,2]
+class(dat_child$V1)
+dat_child[3,2]
+dat_child
+dat_child$V1[[3]]
+##
+##
+##
 
+## use stackexchange soultion from linog
+names(adj_co)
+head(adj_co)
+class(adj_co)
+adj_co <- adj_co[("adj.logic")]
+head(adj_co)
+adj_co_short <- adj_co[,.(list(unique("fips.adjacent"))), by = "fips"]
+## You have many options, depending on how you want to handle them later. 
+## You can use list columns as you proposed, standard lists (use split(dat_child, by = "head") instead), 
+## nested data (either nested data.table or nested tibble). 
+## This is really what you will do later that will determine the most appropriate structure 
 
 ## make number
-adj_co$fips <- as.numeric(adj_co$fips)
-adj_co$fips.adjacent <- as.numeric(adj_co$fips.adjacent)
+# adj_co$fips <- as.numeric(adj_co$fips)
+# adj_co$fips.adjacent <- as.numeric(adj_co$fips.adjacent)
 
 ## only arkansas
-adj_co_ar <- adj_co %>% filter(fips >= 5000, fips < 6000, adj.logic == TRUE)
+# adj_co_ar <- adj_co %>% filter(fips >= 5000, fips < 6000, adj.logic == TRUE)
 
 # mtcars
 # complete(mtcars, cyl, gear, carb)
@@ -125,45 +149,45 @@ adj_co_ar <- adj_co %>% filter(fips >= 5000, fips < 6000, adj.logic == TRUE)
 
 # make graph ##########
 # install.packages("igraph")
-library(igraph)
-test <- adj_co_ar
-# test <- adj_co %>% select(X2, X4)
-gtest <- head(test, 500)
-gtest <- select(gtest, fips, fips.adjacent)
-# gtest <- test %>% select(-list.logic, -UID)
-# gtest <- fill(gtest, county, .direction = "down")
-# gtest <- fill(test, X2, .direction = "down")
-mtest <- as.matrix(gtest)
-class(mtest)
-edge_test <- graph_from_edgelist(mtest)
-class(edge_test)
-# # coords <- layout_(edge_test, as_star())
-# coords <- layout_with_lgl(edge_test)
-# plot.igraph(edge_test, layout = coords)
-
-plot.igraph(edge_test)
-
-head(edge_test)
-edge_json <- toJSON(mtest, matrix = "rowmajor")
-write_json(edge_json, "data/edge_test.json")
+# library(igraph)
+# test <- adj_co_ar
+# # test <- adj_co %>% select(X2, X4)
+# gtest <- head(test, 500)
+# gtest <- select(gtest, fips, fips.adjacent)
+# # gtest <- test %>% select(-list.logic, -UID)
+# # gtest <- fill(gtest, county, .direction = "down")
+# # gtest <- fill(test, X2, .direction = "down")
+# mtest <- as.matrix(gtest)
+# class(mtest)
+# edge_test <- graph_from_edgelist(mtest)
+# class(edge_test)
+# # # coords <- layout_(edge_test, as_star())
+# # coords <- layout_with_lgl(edge_test)
+# # plot.igraph(edge_test, layout = coords)
+# 
+# plot.igraph(edge_test)
+# 
+# head(edge_test)
+# edge_json <- toJSON(mtest, matrix = "rowmajor")
+# write_json(edge_json, "data/edge_test.json")
 
 
 
 
 
 ##
-adj_co <- data.frame(adj_co)
+# adj_co <- data.frame(adj_co)
 
 # rownames(adj_co) <- adj_co$UID
-head(adj_co, 13)
+# head(adj_co, 13)
 ## need adj in column as list
 
 
 # names(adj_co) <- c("")
 ## to JSON
-library(jsonlite)
-co_boundary <- read_json("raw_data/geoJSON_test_boundary.json")
-co_boundary %>% glimpse()
+# library(jsonlite)
+# co_boundary <- read_json("raw_data/geoJSON_test_boundary.json")
+# co_boundary %>% glimpse()
 
 # adj_co_json <- toJSON(adj_co, pretty = FALSE)
 # cat(adj_co_json) %>% head()
